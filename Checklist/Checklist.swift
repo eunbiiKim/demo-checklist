57aef73b720f75d6c02f833a5c1dd7a8ff683963
eunbiiKim <eunbiiKim>
2019-11-22 19:21:37 +0900

    chapter19 - add app icons

--- Checklist/Checklist.swift
+++ Checklist/Checklist.swift
@@ -12,8 +12,9 @@ class Checklist: NSObject, Codable {
     var name = ""
     var iconName = "No Icon"
     var items = [ChecklistItem]()
-    init(name: String) {
+    init(name: String, iconName: String = "No Icon") {
         self.name = name
+        self.iconName = iconName
         super.init()
     }
     
--- Checklist/ListDetailViewController.swift
+++ Checklist/ListDetailViewController.swift
@@ -48,10 +48,12 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate, Icon
             checklist.iconName = iconName
             delegate?.listDetailViewController(self, didFinishEditing: checklist)
         } else {
-            let checklist = Checklist(name: textField.text!)
-            checklist.iconName = iconName
+            let checklist = Checklist(name: textField.text!, iconName: iconName)
             delegate?.listDetailViewController(self, didFinishAdding: checklist)
         }
+        
+        
+        
         navigationController?.popViewController(animated: true)
     }
     
